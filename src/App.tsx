d6a9797c01f2bfac87817365a8ad0d00c913e103
Mger-arzi <mger54598@gmail.com>
2023-10-11 11:11:14 +0800

    5

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,45 @@ import styled from 'styled-components';
 function App() {
     return (
         <div className="App">
-            <Title>Styled-components <span>.attrs</span> method</Title>
 
+
+
+
+
+
+            {/* <Title>Styled-components <span>.attrs</span> method</Title>
             <Form>
+                <CheckBox type='radio'/>
+                <Field type='checkbox'/>
+                <Field type = "radio"/>
                 <Field/>
-                <Field/>
-                <Field/>
-            </Form>
+            </Form> */}
         </div>
     );
 }
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    margin-bottom: 32px;
-    span {
-      color: #e91e63;
-    }
-`;
-
-const Form = styled.form`
-  width: 100%;
-  max-width: 500px;
-`;
-
-const Field = styled.input`
-  padding: 5px 15px;
-  margin: 10px 0;
-  width: 100%;
-  font-size: 1rem;
-`;
+// const Title = styled.h1`
+//     font-size: 1.5em;
+//     text-align: center;
+//     margin-bottom: 32px;
+//     span {
+//       color: #e91e63;
+//     }
+// `;
+// const Form = styled.form`
+//   width: 100%;
+//   max-width: 500px;
+// `;
+// const Field = styled.input`
+//   padding: 5px 15px;
+//   margin: 10px 0;
+//   width: 100%;
+//   font-size: 1rem;
+// `;
+// const CheckBox = styled.input.attrs((props)=>({
+//   type: props.type
+// }) )`
+//   accent-color: #34495e;
+// `
