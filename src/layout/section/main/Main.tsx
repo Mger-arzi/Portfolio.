ce153db4bc10dfba23f56fc063297e09f46b77c7
Mger-arzi <mger54598@gmail.com>
2023-10-12 09:53:59 +0800

    add FlexWrap and Main

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -2,19 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import photo from '../../../accers/image/Mger-1.jpeg'
 import hello from "../../../accers/image/Hi_logo .svg"
+import { FlexWrapper } from '../../../componenc/FlexWrapper';
 export  const Main = () => {
 	return (
-		<div>
-			<div>
-				<span>Привет<Hello src={hello}/></span><br></br>
-				<span>Меня зовут</span>
-				<h2>Мгер Арзикян</h2>
-				<h1>Я Front-end разработчик</h1>
-			</div>
-			<Photo src={photo} alt="" />
-		</div>
+		<StyledMain>
+			<FlexWrapper align={'center'} justify=' space-around '>
+				<div>
+					<SpanHello>Привет<Hello src={hello}/></SpanHello><br></br>
+					<SpanName>Меня зовут</SpanName>
+					<MyName>Мгер Арзикян</MyName>
+					<IFrontEnder>Я Front-end разработчик</IFrontEnder>
+				</div>
+
+				<Photo src={photo} alt="" />
+			</FlexWrapper>
+			
+		</StyledMain>
 	);
 };
+
+const  StyledMain = styled.div`
+min-height: 100vh;
+background-color: #191919;
+`
+
+
 const Hello = styled.img`
 	width: 20px;
 	height: 20px;
@@ -26,4 +38,17 @@ const Photo = styled.img`
 	border-radius: 50%;
 	border: 4px solid  #00C0FD;
 	//#E70FAA
+`
+const SpanHello = styled.span`
+	
+`
+
+const SpanName = styled.span`
+	
+`
+const MyName = styled.h2`
+	
+`
+const IFrontEnder = styled.h1`
+	
 `
