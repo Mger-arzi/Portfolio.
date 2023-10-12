d87bc9daafb4104ea2bdbaae8ea8b72b035253f0
Mger-arzi <mger54598@gmail.com>
2023-10-12 16:50:56 +0300

    add work

--- src/layout/section/works/Works.tsx
+++ src/layout/section/works/Works.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { SectionDec, SectionTitle } from '../../../componenc/SectionTitle';
 import { FlexWrapper } from '../../../componenc/FlexWrapper';
-
+import { Work } from './work/Work';
+import PhotoWork1 from "../../../accers/image/Rectangle 22.jpg"
 
 
 export const Works = () => {
@@ -10,8 +11,31 @@ export const Works = () => {
 		<StyledWorks>
 			<SectionTitle>Проекты</SectionTitle>
 			<SectionDec>Вещи, которые я построил на данный момент</SectionDec>
-			<FlexWrapper>
-				
+			<FlexWrapper justify='space-between' wrap='wrap'>
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here'
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here' 
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here' 
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here' 
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here' 
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
+				<Work src={PhotoWork1 }
+					title='Project Tile goes here' 
+					text='This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content'
+					stack='HTML , JavaScript, SASS, React' />
 			</FlexWrapper>
 		</StyledWorks>
 	);
--- src/layout/section/works/work/Work.tsx
+++ src/layout/section/works/work/Work.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const Work = () => {
+type WorkPropsType = {
+	title: string
+	text: string
+	src: string
+	stack: string
+}
+
+export const Work = (props: WorkPropsType) => {
 	return (
-		<div>
-			
-		</div>
+		<SteledWork>
+			<Image src={props.src} alt="" />
+			<Title>{props.title}</Title>
+			<Text>{props.text}</Text>
+			<TechStack>Мой стек: {props.stack}</TechStack>
+			<Link href='#'>Открыть в браузере</Link> 
+			<Link href='#'>Просмотреть код</Link>
+		</SteledWork>
 	);
 };
 
+const SteledWork = styled.div`
+	background-color: #ebe767;
+	max-width: 375px;
+	width: 100%;
+`
+const Image = styled.img`
+	width: 100%;
+	height: 260px;
+	object-fit: cover;
+`
+const Link = styled.a`
+	
+`
+const Title = styled.h3`
+	
+`
+const Text = styled.p`
+	
+`
+const TechStack = styled.p`
+	
+`
