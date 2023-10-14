7b03689c4fed400cb67fe83d40ec11d9a531b3fc
Mger-arzi <mger54598@gmail.com>
2023-10-14 19:46:50 +0300

    add footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FlexWrapper } from '../../componenc/FlexWrapper';
+import { Logo } from '../../componenc/logo/Logo';
 
 export const Footer = () => {
 	return (
 		<StyledFooter>
-			
+			<FlexWrapper>
+				<Logo/>
+				
+			</FlexWrapper>
 		</StyledFooter>
 	);
 };
